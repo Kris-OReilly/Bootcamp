c4afd7eb0fd63cd0430e11cbe5f09b7e3991f7ae
[Kris O'Reilly] <reilly16988@gmail.com>
2024-03-07 20:21:56 +0000

    Added while loop and error handling for user input

--- award.py
+++ award.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # create a variable input for swimming time, 
 # a variable input for cycling time,
 # a variable input for running time.
@@ -10,11 +10,17 @@
 # elif 106 <= total_time <= 110; the award will be 'Provincial scroll'
 # else no award given
 
-swimming_time = int(input("Enter swim time in whole minutes:"))
-cycling_time = int(input("Enter cycling time in whole minutes:"))
-running_time = int(input("Enter running time in whole minutes:"))
-# our 3 event completion times need to be inputted to calculate the total time 
-
+while True:
+    try:
+        swimming_time = int(input("Enter swim time in whole minutes:"))
+        cycling_time = int(input("Enter cycling time in whole minutes:"))
+        running_time = int(input("Enter running time in whole minutes:"))
+        break
+        # our 3 event completion times need to be inputted to calculate the total time 
+    except ValueError:
+        print("You did not enter a valid number, try again")
+           
+    
 total_time = swimming_time + cycling_time + running_time # variable to add the three inputted times together
 print(f"Your triathlon completion time is: {total_time} minutes") # using an f string to display triathlon completion time
 
@@ -26,33 +32,4 @@ elif total_time >= 101 and total_time <= 105: # checks if the value of total_tim
 elif total_time >= 106 and total_time <= 110: # checks if the value of total_time is between 106-110 minutes to return True
     print("You won a Provincial Scroll!")
 else:    # if the value of total_time is any value 111 and above this will return True
-=======
-# create a variable input for swimming time, 
-# a variable input for cycling time,
-# a variable input for running time.
-# create a variable for total_time which calculates the total of all 3 inputted times.
-# print the total time for completion
-# print the award the participant will receive using an if-else condition
-# if total_time <= 100, the award will be 'Provincial colours'
-# elif 101 <= total_time <= 105; the award will be 'Provincial half colours'
-# elif 106 <= total_time <= 110; the award will be 'Provincial scroll'
-# else no award given
-
-swimming_time = int(input("Enter swim time in whole minutes:"))
-cycling_time = int(input("Enter cycling time in whole minutes:"))
-running_time = int(input("Enter running time in whole minutes:"))
-# our 3 event completion times need to be inputted to calculate the total time 
-
-total_time = swimming_time + cycling_time + running_time # variable to add the three inputted times together
-print(f"Your triathlon completion time is: {total_time} minutes") # using an f string to display triathlon completion time
-
-
-if total_time <= 100: # checks if the value of total_time is less than 100 minutes to return True
-    print("You won Provincial Colours!")
-elif total_time >= 101 and total_time <= 105: # checks if the value of total_time is between 101-105 minutes to return True
-    print("You won Provincial Half Colours!")
-elif total_time >= 106 and total_time <= 110: # checks if the value of total_time is between 106-110 minutes to return True
-    print("You won a Provincial Scroll!")
-else:    # if the value of total_time is any value 111 and above this will return True
->>>>>>> a2a808354ee9571f3dc6a535263b0b26bc315446
     print("Sorry, your time did not qualify for an award, better luck next time")
